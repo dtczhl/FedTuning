7c315c03411f564f51915cc5bed034c9b07b4f69
Huanle Zhang <zhanghuanle1342@gmail.com>
2021-11-18 00:16:12 -0800

    Add running script

---
+++ FedTuning/experiment.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+# path to this git
+path_to_git=/home/dtczhl/FedTuning
+# change your virtual environment name
+conda_env_name=fedtuning
+
+# Copy the below from your ~/.bashrc
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/dtczhl/Software/Anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/dtczhl/Software/Anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/dtczhl/Software/Anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/dtczhl/Software/Anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+(
+cd $path_to_git/
+conda activate ${conda_env_name}
+CUDA_VISIBLE_DEVICES=0 nohup python -u FedTuning/main.py --enable_fedtuning True --alpha 0 --beta 0.5 --gamma 0.5 --delta 0 --model resnet_10 --target_model_accuracy 0.8 --n_participant 20 --n_training_pass 20 --dataset speech_command --penalty 2 &> Log/log_2 &
+CUDA_VISIBLE_DEVICES=1 nohup python -u FedTuning/main.py --enable_fedtuning True --alpha 0 --beta 0.5 --gamma 0.5 --delta 0 --model resnet_10 --target_model_accuracy 0.8 --n_participant 20 --n_training_pass 20 --dataset speech_command --penalty 3 &> Log/log_3 &
+CUDA_VISIBLE_DEVICES=2 nohup python -u FedTuning/main.py --enable_fedtuning True --alpha 0 --beta 0.5 --gamma 0.5 --delta 0 --model resnet_10 --target_model_accuracy 0.8 --n_participant 20 --n_training_pass 20 --dataset speech_command --penalty 4 &> Log/log_4 &
+CUDA_VISIBLE_DEVICES=3 nohup python -u FedTuning/main.py --enable_fedtuning True --alpha 0 --beta 0.5 --gamma 0.5 --delta 0 --model resnet_10 --target_model_accuracy 0.8 --n_participant 20 --n_training_pass 20 --dataset speech_command --penalty 5 &> Log/log_5 &
+CUDA_VISIBLE_DEVICES=4 nohup python -u FedTuning/main.py --enable_fedtuning True --alpha 0 --beta 0.5 --gamma 0.5 --delta 0 --model resnet_10 --target_model_accuracy 0.8 --n_participant 20 --n_training_pass 20 --dataset speech_command --penalty 6 &> Log/log_6 &
+CUDA_VISIBLE_DEVICES=5 nohup python -u FedTuning/main.py --enable_fedtuning True --alpha 0 --beta 0.5 --gamma 0.5 --delta 0 --model resnet_10 --target_model_accuracy 0.8 --n_participant 20 --n_training_pass 20 --dataset speech_command --penalty 7 &> Log/log_7 &
+CUDA_VISIBLE_DEVICES=6 nohup python -u FedTuning/main.py --enable_fedtuning True --alpha 0 --beta 0.5 --gamma 0.5 --delta 0 --model resnet_10 --target_model_accuracy 0.8 --n_participant 20 --n_training_pass 20 --dataset speech_command --penalty 8 &> Log/log_8 &
+CUDA_VISIBLE_DEVICES=7 nohup python -u FedTuning/main.py --enable_fedtuning True --alpha 0 --beta 0.5 --gamma 0.5 --delta 0 --model resnet_10 --target_model_accuracy 0.8 --n_participant 20 --n_training_pass 20 --dataset speech_command --penalty 9 &> Log/log_9 &
+CUDA_VISIBLE_DEVICES=0 nohup python -u FedTuning/main.py --enable_fedtuning True --alpha 0 --beta 0.5 --gamma 0.5 --delta 0 --model resnet_10 --target_model_accuracy 0.8 --n_participant 20 --n_training_pass 20 --dataset speech_command --penalty 10 &> Log/log_10 &
+)
+
+
--- ResultAnalysis/penalty_performance.py
+++ ResultAnalysis/penalty_performance.py
@@ -13,12 +13,12 @@ import matplotlib.pyplot as plt
 improvement_ratio = [
     [10.99, 14.21, 13.58, 16.95, 0.47, 11.29, 4.13, 12.73, 11.44, 16.66],
     [6.36, -0.23, 21.79, -3.58, 14.4, 11.52, -1.21, 13.26, 19.30, -1.62],
-    # [-57.73, 10.87, 19.22, 34.24, 28.50, 12.37, 18.18, 5.99, 16.29, 19.16]
+    [-36.76, -3.61, 5.57, -7.13, 5.27, 15.95, 5.47, -3.18, 10.07, 17.43]
 ]
 trace_legend = [
     '(0.25, 0.25, 0.25, 0.25)',
     '(0, 0.33, 0.33, 0.33)',
-    # '(0.33, 0, 0.33, 0.33)'
+    '(0, 0.5, 0, 0.5)'
 ]
 penalty_factor = np.arange(1, len(improvement_ratio[0])+1)
 
@@ -29,13 +29,13 @@ sys.path.append(project_dir)
 plt.figure(1, figsize=(6, 5))
 plt.plot(penalty_factor, improvement_ratio[0], '-o', linewidth=3)
 plt.plot(penalty_factor, improvement_ratio[1], '-^', linewidth=3)
-# plt.plot(penalty_factor, improvement_ratio[2], '-*', linewidth=3)
-plt.legend(trace_legend, loc='upper center', fontsize=20)
+plt.plot(penalty_factor, improvement_ratio[2], '-*', linewidth=3)
+plt.legend(trace_legend, loc='lower center', fontsize=20)
 plt.xlabel('Penalty', fontsize=24)
 plt.xlim([1, 10])
 plt.xticks(penalty_factor, fontsize=22)
 plt.ylabel('Improvement Ratio (%)', fontsize=24)
-plt.ylim([-10, 40])
+plt.ylim([-40, 30])
 # plt.yticks(np.arange(-60, 50, 20), fontsize=22)
 plt.yticks(fontsize=22)
 plt.grid(linestyle='--', linewidth=0.2)
